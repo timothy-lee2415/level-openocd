aad3c571f8bccc6d275648c9a3c33853024c9c12
Timothy Lee <timothy.lee@level.co>
2023-05-30 14:40:10 -0700

    add silabs flash driver to openocd

--- src/flash/nor/driver.h
+++ src/flash/nor/driver.h
@@ -255,6 +255,7 @@ extern const struct flash_driver cc3220sf_flash;
 extern const struct flash_driver cfi_flash;
 extern const struct flash_driver dsp5680xx_flash;
 extern const struct flash_driver efm32_flash;
+extern const struct flash_driver efm32s2_flash;
 extern const struct flash_driver em357_flash;
 extern const struct flash_driver esirisc_flash;
 extern const struct flash_driver faux_flash;
